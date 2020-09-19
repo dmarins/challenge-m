9b1bc7730cb2e3a1f5ce7c468dce9316010af27a
Diogo da Silva França Marins <dmarins@braspag.com.br>
2020-09-19 17:09:15 -0300

    middleware para tratamento de exceções inesperadas + testes

---
+++ src/M.Challenge/M.Challenge.Api/Infrastructure/Middlewares/UnexpectedErrorHandlingMiddleware.cs
@@ -0,0 +1,66 @@
+﻿using M.Challenge.Api.Infrastructure.ProblemDetails;
+using M.Challenge.Domain.Logger;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace M.Challenge.Api.Infrastructure.Middlewares
+{
+    public class UnexpectedErrorHandlingMiddleware
+    {
+        public RequestDelegate Next { get; }
+        public ILogger Logger { get; }
+
+        public UnexpectedErrorHandlingMiddleware(RequestDelegate next, ILogger logger)
+        {
+            Next = next ?? throw new ArgumentNullException(nameof(next));
+            Logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await Next(context);
+            }
+            catch (Exception ex)
+            {
+                Logger.Error("An unexpected error occurred on the server.", ex);
+                await HandleException(context, ex);
+            }
+        }
+
+        private static Task HandleException(HttpContext context, Exception ex)
+        {
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            context.Response.ContentType = "application/problem+json";
+
+            var result = JsonConvert
+                .SerializeObject(
+                    new InternalServerErrorProblemDetails
+                    {
+                        Detail = ex.Message
+                    },
+                    Formatting.Indented,
+                    new JsonSerializerSettings
+                    {
+                        NullValueHandling = NullValueHandling.Ignore,
+                        ContractResolver = new DefaultContractResolver()
+                    });
+
+            return context.Response.WriteAsync(result);
+        }
+    }
+
+    public static class UnexpectedErrorHandlingMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseUnexpectedErrorHandlingMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<UnexpectedErrorHandlingMiddleware>();
+        }
+    }
+}
---
+++ src/M.Challenge/M.Challenge.Api/Infrastructure/ProblemDetails/InternalServerErrorProblemDetails.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+
+namespace M.Challenge.Api.Infrastructure.ProblemDetails
+{
+    public class InternalServerErrorProblemDetails : Microsoft.AspNetCore.Mvc.ProblemDetails
+    {
+        public InternalServerErrorProblemDetails(string details = null)
+        {
+            Title = "InternalServerError";
+            Detail = details;
+            Status = (int)HttpStatusCode.InternalServerError;
+            Type = "https://httpstatuses.com/500";
+        }
+    }
+}
