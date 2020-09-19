4f18208b35622076f6d60d6efef4e2c763ca3846
Diogo da Silva França Marins <dmarins@braspag.com.br>
2020-09-19 16:15:52 -0300

    adicionando e configurando o serilog

--- src/M.Challenge/M.Challenge.Api/Program.cs
+++ src/M.Challenge/M.Challenge.Api/Program.cs
@@ -1,26 +1,46 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using System.Diagnostics.CodeAnalysis;
 
 namespace M.Challenge.Api
 {
+    [ExcludeFromCodeCoverage]
     public class Program
     {
+        private static string _environmentName;
+
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var webHost = BuildWebHost(args);
+
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile($"appsettings.{_environmentName}.json", optional: true, reloadOnChange: true)
+                .Build();
+
+            Log.Logger = new LoggerConfiguration().ReadFrom
+                .Configuration(configuration)
+                .CreateLogger();
+
+            webHost.Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            return WebHost
+                .CreateDefaultBuilder(args)
+                .ConfigureLogging(
+                    (hostingContext, config) =>
+                    {
+                        config.ClearProviders();
+                        _environmentName = hostingContext.HostingEnvironment.EnvironmentName;
+                    })
+                .UseStartup<Startup>()
+                .Build();
+        }
     }
 }
---
+++ src/M.Challenge/M.Challenge.Infrastructure/Logger/Logger.cs
@@ -0,0 +1,25 @@
+﻿using Serilog;
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace M.Challenge.Infrastructure.Logger
+{
+    [ExcludeFromCodeCoverage]
+    public class Logger
+    {
+        public void Info(string message, params object[] propertyValues)
+        {
+            Log.Information(message, propertyValues);
+        }
+
+        public void Error(string message, Exception exception, params object[] propertyValues)
+        {
+            Log.Error(exception, message, propertyValues);
+        }
+
+        public void Warning(string message, Exception exception = null, params object[] propertyValues)
+        {
+            Log.Warning(exception, message, propertyValues);
+        }
+    }
+}
